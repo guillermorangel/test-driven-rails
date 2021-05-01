7d92a658e5a2258f51f675a4bdc3643896f64718
guillermorangel <guillermo.m.rangel@gmail.com>
2021-05-01 16:25:18 -0400

    Spec for new authenticated session

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "todos#index"
 
   resources :todos, only: [:index, :new, :create]
+  resource :session, only: [:new]
 end
--- spec/support/features/sign_in.rb
+++ spec/support/features/sign_in.rb
@@ -1,5 +1,8 @@
 module Features
   def sign_in
     visit root_path
+
+    fill_in "Email", with: "person@example.com"
+    click_on "Sign In"
   end
 end
