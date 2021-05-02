f6ee54c56cc8e40e8e5afa48f99e96a56de3e8c4
guillermorangel <guillermo.m.rangel@gmail.com>
2021-05-02 09:23:56 -0400

    Add email attribute to todos
    $ rails g migration add_email_to_todo
    $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_020024) do
+ActiveRecord::Schema.define(version: 2021_05_02_132129) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
