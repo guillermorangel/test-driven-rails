0a75519b39dc50a2fa43d00ae5ae161d36ed8615
guillermorangel <guillermo.m.rangel@gmail.com>
2021-05-28 17:29:24 -0400

    Refactor session current email into application controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,11 +2,11 @@ class TodosController < ApplicationController
   before_action :authenticate
 
   def index
-    @todos = Todo.where(email: session[:current_email])
+    @todos = Todo.where(email: current_email)
   end
 
   def create
-    Todo.create(todo_params.merge(email: session[:current_email]))
+    Todo.create(todo_params.merge(email: current_email))
     redirect_to todos_path
   end
 
