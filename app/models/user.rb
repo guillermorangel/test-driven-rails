8790d7031ab2f42ef6f7635e609878d9da2abfe7
guillermo <guillermo.m.rangel@gmail.com>
2021-05-30 14:30:53 -0400

    Create User model and refactor to rely on model logic

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class User
+  def initialize(email)
+    @email = email
+  end
+
+  def todos
+    Todo.where(email: @email)
+  end
+
+  def signed_in?
+    @email.present?
+  end
+end
